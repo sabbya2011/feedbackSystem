309a8adf043bce935ffdfc66a14ccbd0e48ee270
Sabbyasachi Roy <sabbya2011@gmail.com>
2018-07-02 15:04:01 +0530

    First Commit of Feedback Server code

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/req',(req,res)=>{
+    res.send({ping:'server is ready'});
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log("Feedback server is up and running");
+});
