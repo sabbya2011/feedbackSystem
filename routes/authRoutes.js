be869cbdfe3db2b2c071141550a223323d274f06
Sabbyasachi Roy <sabbya2011@gmail.com>
2018-07-09 16:55:15 +0530

    remove default route from express

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const passport = require('passport');
 
 module.exports = app =>{
-    app.get('/',(req,res)=>{
-        res.send({ping:'server is ready'});
-    });
+    // app.get('/',(req,res)=>{
+    //     res.send({ping:'server is ready'});
+    // });
     
     app.get('/auth/google',passport.authenticate('google',{
         scope:['profile','email']
